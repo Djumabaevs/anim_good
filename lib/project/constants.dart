5d11a8e050227c449bf142739b8586b2ef834385
Djumabaevs <djumabaevb@gmail.com>
2022-05-02 12:40:20 +0600

    constants

---
+++ lib/project/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+// ignore: constant_identifier_names
+const Color APP_COLOR = Color(0xff5b3bfe);
+
+// ignore: constant_identifier_names
+const int PAGE_LIMIT = 10;
+
+enum SortTypes {
+  // ignore: constant_identifier_names
+  ASC,
+  // ignore: constant_identifier_names
+  DESC,
+}
+
+enum GetTypes {
+  // ignore: constant_identifier_names
+  FILTER,
+  // ignore: constant_identifier_names
+  PAGING,
+}
