3984fa730bdd909dc38481d77d25bb6cd186a546
Djumabaevs <djumabaevb@gmail.com>
2022-05-07 12:29:50 +0600

    api service

---
+++ lib/project/api_service.dart
@@ -0,0 +1,52 @@
+class APIService {
+  final String _baseUrl = "http://localhost:8080/api";
+  Map<String, String> headers = {
+    "content-type": "application/json",
+    "accept": "application/json",
+  };
+
+  Future<http.Response> get(String url, Map<String, String> params) async {
+    try {
+      Uri uri = Uri.parse(_baseUrl + url).replace(queryParameters: params);
+      // params /*String|Iterable<String>*/
+      http.Response response = await http.get(uri);
+      return response;
+    } catch (e) {
+      return http.Response({"message": e}.toString(), 400);
+    }
+  }
+
+  Future<http.Response> post(String url, Map<String, dynamic> body) async {
+    try {
+      Uri uri = Uri.parse(_baseUrl + url);
+      String bodyString = json.encode(body);
+      http.Response response =
+          await http.post(uri, headers: headers, body: bodyString);
+      return response;
+    } catch (e) {
+      return http.Response({"message": e}.toString(), 400);
+    }
+  }
+
+  Future<http.Response> put(String url, Map<String, dynamic> body) async {
+    try {
+      Uri uri = Uri.parse(_baseUrl + url);
+      String bodyString = json.encode(body);
+      http.Response response =
+          await http.put(uri, headers: headers, body: bodyString);
+      return response;
+    } catch (e) {
+      return http.Response({"message": e}.toString(), 400);
+    }
+  }
+
+  Future<http.Response> delete(String url) async {
+    try {
+      Uri uri = Uri.parse(_baseUrl + url);
+      http.Response response = await http.delete(uri, headers: headers);
+      return response;
+    } catch (e) {
+      return http.Response({"message": e}.toString(), 400);
+    }
+  }
+}
