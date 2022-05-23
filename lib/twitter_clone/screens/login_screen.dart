3ff7162842a4960cdda5a0be630d0b286edd77b0
Djumabaevs <djumabaevb@gmail.com>
2022-05-23 09:42:22 +0600

    login screen

---
+++ lib/twitter_clone/screens/login_screen.dart
@@ -0,0 +1,70 @@
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email;
+  String _password;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: KTweeterColor,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Log in',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter Your email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Enter Your password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'LOG IN',
+              onBtnPressed: () async {
+                bool isValid = await AuthService.login(_email, _password);
+                if (isValid) {
+                  Navigator.pop(context);
+                } else {
+                  print('login problem');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
