0a2c67f84859ef249bb14663ca3412d19f2d2394
Djumabaevs <djumabaevb@gmail.com>
2022-05-26 00:00:21 +0600

    registration screen

---
+++ lib/twitter_clone/screens/registration_screen.dart
@@ -0,0 +1,83 @@
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  String _email;
+  String _password;
+  String _name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: KTweeterColor,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Registration',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Name',
+              ),
+              onChanged: (value) {
+                _name = value;
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'Create account',
+              onBtnPressed: () async {
+                bool isValid =
+                    await AuthService.signUp(_name, _email, _password);
+                if (isValid) {
+                  Navigator.pop(context);
+                } else {
+                  print('something wrong');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
