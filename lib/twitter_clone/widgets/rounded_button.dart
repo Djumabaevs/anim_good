5ada66196c0f5d9c80f1514a82896474ff8ef9bd
Djumabaevs <djumabaevb@gmail.com>
2022-06-01 10:30:39 +0600

    rounded button

---
+++ lib/twitter_clone/widgets/rounded_button.dart
@@ -0,0 +1,24 @@
+class RoundedButton extends StatelessWidget {
+  final String btnText;
+  final Function onBtnPressed;
+
+  const RoundedButton({Key key, this.btnText, this.onBtnPressed})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5,
+      color: KTweeterColor,
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        onPressed: onBtnPressed,
+        minWidth: 320,
+        height: 60,
+        child: Text(
+          btnText,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
